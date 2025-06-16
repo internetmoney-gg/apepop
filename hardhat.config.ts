d2d605f7b2fbbd38baebeb0e7a4ec1babdf94b4d
advaith101 <advaith101@gmail.com>
2025-06-16 17:55:13 -0400

    hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,13 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+      viaIR: true
+    }
+  },
   // allowUnlimitedContractSize: true,
   networks: {
     localhost: {
